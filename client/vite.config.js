e2271df272d33f4a016b3bc2fbffc6bec638376a
Manab <paul.manabendra2002@gmail.com>
2026-08-14 01:18:39 +0530

    Target modification

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        // target: 'http://localhost:5000',
+        target: 'https://careergraph-backend-dgfa.onrender.com',
         changeOrigin: true,
       }
     }
